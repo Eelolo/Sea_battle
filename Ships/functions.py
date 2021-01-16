89aceeb38b76457dbc34749635483921887a50c1
Eelolo <Eelolo@yandex.ru>
2021-01-16 12:32:32 +0300

    new ship function refatoring

--- Ships/functions.py
+++ Ships/functions.py
@@ -1,41 +1,45 @@
 from random import choice, randint
+from sea_battle.battlefield.classes import Cursor
 
 
-def new_ship(length, **kwargs):
-    try:
-        if kwargs['orientation'] not in ('hor', 'vert'):
-            raise ValueError('Orientation must be "hor" or "vert".')
-        orientation = kwargs['orientation']
-    except KeyError:
+def new_ship(length, orientation=None, start_point=None):
+    if orientation is not None and orientation in ('hor', 'vert'):
+        orientation = orientation
+    elif orientation is None:
         orientation = choice(('hor', 'vert'))
+    else:
+        raise ValueError('Orientation must be "hor" or "vert".')
 
-    try:
-        if kwargs['start_point'][0] not in range(1,11) and ord(kwargs['start_point'][1]) not in range(97, 106):
-            raise ValueError('Start point must be like "1a" or "6d", in range 1a-10j.')
-        integer = int(kwargs['start_point'][0])
-        letter = ord(kwargs['start_point'][1])
-    except KeyError:
+    if start_point is not None:
+        if int(start_point[0]) in range(1, 11) and ord(start_point[1]) in range(97, 107):
+            if '10' in start_point:
+                integer = 10
+            else:
+                integer = int(start_point[0])
+            letter = ord(start_point[1])
+    elif start_point is None:
         integer = randint(1, 10)
         letter = randint(97, 106)
+        start_point = str(integer) + chr(letter)
+    else:
+        raise ValueError('Start point must be like "1a" or "6d", in range 1a-10j.')
 
     ship = []
+    cur = Cursor(start_point=start_point)
+    ship.append(start_point)
     if orientation == 'hor':
         if letter > 106 - length:
-            for _ in range(length):
-                ship.append(str(integer) + chr(letter))
-                letter -= 1
+            for _ in range(length - 1):
+                ship.append(cur.move('left'))
         else:
-            for _ in range(length):
-                ship.append(str(integer) + chr(letter))
-                letter += 1
+            for _ in range(length - 1):
+                ship.append(cur.move('right'))
     elif orientation == 'vert':
         if integer > 10 - length:
-            for _ in range(length):
-                ship.append(str(integer) + chr(letter))
-                integer -= 1
+            for _ in range(length - 1):
+                ship.append(cur.move('up'))
         else:
-            for _ in range(length):
-                ship.append(str(integer) + chr(letter))
-                integer += 1
+            for _ in range(length - 1):
+                ship.append(cur.move('down'))
 
     return ship
