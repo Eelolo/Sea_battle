18fd6595f2c83a4eed0053a124fde8ae43dd56a1
Eelolo <Eelolo@yandex.ru>
2021-01-16 16:03:23 +0300

    added right point checking

--- Ships/functions.py
+++ Ships/functions.py
@@ -1,6 +1,6 @@
 from random import choice, randint
 from sea_battle.battlefield.classes import Cursor
-
+from sea_battle.battlefield.functions import check_point_value
 
 def new_ship(length, orientation=None, start_point=None):
     if orientation is not None and orientation in ('hor', 'vert'):
@@ -11,18 +11,16 @@ def new_ship(length, orientation=None, start_point=None):
         raise ValueError('Orientation must be "hor" or "vert".')
 
     if start_point is not None:
-        if int(start_point[0]) in range(1, 11) and ord(start_point[1]) in range(97, 107):
-            if '10' in start_point:
-                integer = 10
-            else:
-                integer = int(start_point[0])
-            letter = ord(start_point[1])
-    elif start_point is None:
+        check_point_value(start_point)
+        if '10' in start_point:
+            integer = 10
+        else:
+            integer = int(start_point[0])
+        letter = ord(start_point[1])
+    else:
         integer = randint(1, 10)
         letter = randint(97, 106)
         start_point = str(integer) + chr(letter)
-    else:
-        raise ValueError('Start point must be like "1a" or "6d", in range 1a-10j.')
 
     ship = []
     cur = Cursor(start_point=start_point)
