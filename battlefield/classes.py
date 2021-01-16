a4d66ccc1e8fe648b9e5f55d2b86180c1e7d1661
Eelolo <Eelolo@yandex.ru>
2021-01-16 20:03:15 +0300

    added attributes for around ship function

--- battlefield/classes.py
+++ battlefield/classes.py
@@ -35,6 +35,18 @@ class Cursor:
             self.point = '1a'
         self._battlefield.change_value(self.point, 'X')
         self._point_key_idx = self._field_keys.index(self.point)
+        self._reversed_moves = {
+            'right': 'left',
+            'left': 'right',
+            'down': 'up',
+            'up': 'down'
+        }
+        self._perpendicular_moves = {
+            'right': ('up', 'down'),
+            'left': ('up', 'down'),
+            'down': ('right', 'left'),
+            'up': ('right', 'left')
+        }
 
     def __setattr__(self, key, value):
         if key == 'point':
