26dadc81a8430b2db4b187545ae9123e277e0a7e
Eelolo <Eelolo@yandex.ru>
2021-01-16 20:01:25 +0300

    area around ship function

--- Ships/functions.py
+++ Ships/functions.py
@@ -41,3 +41,46 @@ def new_ship(length, orientation=None, start_point=None):
                 ship.append(cur.down())
 
     return ship
+
+
+def area_around_ship(ship):
+    cur = Cursor()
+    length = len(ship)
+    if len(ship) == 1:
+        point_difference = 1
+    else:
+        point_difference = cur._field_keys.index(ship[1]) - cur._field_keys.index(ship[0])
+
+    if point_difference in (1, -1):
+        if point_difference > 0:
+            cur_method = 'right'
+        else:
+            cur_method = 'left'
+    else:
+        if point_difference > 0:
+            cur_method = 'down'
+        else:
+            cur_method = 'up'
+
+    around_ship = []
+    break_on = None
+    for loop_idx in range(3):
+        cur.move(ship[0])
+
+        if loop_idx == 0:
+            around_ship.append(getattr(cur, cur._reversed_moves[cur_method])())
+        else:
+            getattr(cur, cur._reversed_moves[cur_method])()
+            around_ship.append(getattr(cur, cur._perpendicular_moves[cur_method][loop_idx - 1])())
+
+        if cur.point == ship[0]:
+            break_on = length - 1
+
+        for move_idx in range(length + 1):
+            around_ship.append(getattr(cur, cur_method)())
+            if break_on is not None and move_idx == break_on:
+                break
+
+    around_ship = list(set(around_ship) - set(ship))
+
+    return around_ship
