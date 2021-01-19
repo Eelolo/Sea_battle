27148568462689f58e34022e6262420d1b75b6d0
Eelolo <Eelolo@yandex.ru>
2021-01-19 17:48:55 +0300

    Game class initial structure

---
+++ Gameplay/classes.py
@@ -0,0 +1,20 @@
+from battlefield.classes import Battlefield
+
+class Game:
+    player_battlefield = Battlefield()
+    opponent_battlefield = Battlefield()
+
+    def place_ships(self):
+        pass
+
+    def realize_move(self):
+        pass
+
+    def visualize_player_ships_placing(self):
+        pass
+
+    def visualize_playing(self):
+        pass
+    
+    def start(self):
+        pass
