0f238c59779bfc12288c024b8e5ef89ced9f0e9f
Eelolo <Eelolo@yandex.ru>
2021-01-19 19:57:01 +0300

    defining ships: added variables

---
+++ config/templates/defining_ships.py
@@ -0,0 +1,11 @@
+BATTLEFIELD = '\n       Your battlefield\n' \
+              '    a b c d e f g h i j\n' \
+              '{}'
+
+EXPLANATIONS = {
+    'fourdeck': 'Place the battleship. Length - 4 points.\nSet points like "3b,3c,3d,3e" or "2b,3b,4b,5b".\n',
+    'tripledecks': 'Place two cruisers. Length - 3 points.\nPlace first, then the second.\n',
+    'doubledecks': 'Place three destroyers. Length - 2 points.\n',
+    'singledecks': 'Place four torpedo boats. Length - 1 point.\n'
+}
+
