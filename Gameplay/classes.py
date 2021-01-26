a699d9aa993c9dcc85743ecbb4e54cd839fb686b
Eelolo <Eelolo@yandex.ru>
2021-01-26 05:20:56 +0300

    opponent logic

--- Gameplay/classes.py
+++ Gameplay/classes.py
@@ -1,10 +1,14 @@
-from battlefield.classes import Battlefield
+from battlefield.classes import Battlefield, Cursor
 from ships.classes import Ship
 from ships.functions import random_ships_set, check_for_matches
-from config.config import SHIPS_EMPTY_SET, SHIPS_ATTR_NAMES
+from config.config import (
+    SHIPS_EMPTY_SET, SHIPS_ATTR_NAMES, METHODS, REVERSED_MOVES, PERPENDICULAR_MOVES, SEARCH_PLAN
+)
 from config.templates.defining_ships import BATTLEFIELD, EXPLANATIONS
 from .functions import check_length, is_straight_check, points_in_field_keys_check
+from random import choice
 import os
+import time
 
 
 class Player:
@@ -24,8 +28,7 @@ class Player:
 
 
 class Opponent:
-    played_moves = []
-    last_move = ''
+    search_plan = SEARCH_PLAN
 
     def ships_placing(self):
         ships = random_ships_set()
@@ -37,20 +40,120 @@ class Opponent:
                 setattr(self, ship_attr_name, ship)
 
     def __init__(self):
-        self.ships = random_ships_set()
+        self.status = 'search'
+        self.last_move_result = ''
+        self.discarded_points = []
+        self.cursor = Cursor()
+        self.cur_methods = []
+        self.discarded_methods = []
+        self.area_to_exclude = []
+        self.last_move_point = ''
+        self.waits_to_discard = []
+
         self.ships_placing()
 
+    def search(self):
+        if self.search_plan:
+            while self.search_plan[-1] in self.discarded_points:
+                self.search_plan.pop(-1)
+                if not self.search_plan:
+                    break
+
+        if not self.search_plan:
+            move = self.random_point()
+        else:
+            move = self.search_plan.pop(-1)
+            self.discarded_points.append(move)
+
+        return move
+
+    def regulation(self):
+        if self.last_move_result == 'Damaged.' and self.cur_methods:
+            self.cur_methods.append(self.cur_methods[-1])
+
+        for method in self.cur_methods:
+            if self.cur_methods.count(method) >= 2:
+                self.discarded_methods.extend(PERPENDICULAR_MOVES[method])
+                if self.cursor.check_move_result(self.cur_methods[-1]) == self.cursor.point:
+                    self.last_move_result = 'Miss.'
+                    self.cursor.move(self.discarded_points[-1])
+
+        for method in METHODS:
+            if self.cursor.check_move_result(method) in self.discarded_points[:-1]:
+                self.discarded_methods.append(method)
+
+        if '1' in self.cursor.point and '10' not in self.cursor.point:
+            self.discarded_methods.append('up')
+        elif '10' in self.cursor.point:
+            self.discarded_methods.append('down')
+
+        if 'a' in self.cursor.point:
+            self.discarded_methods.append('left')
+        elif 'j' in self.cursor.point:
+            self.discarded_methods.append('right')
+
+    def destruction(self):
+        if self.last_move_result == 'Damaged.':
+            start_point = self.last_move_point
+        else:
+            start_point = self.discarded_points[-1]
+
+        self.cursor.move(start_point)
+
+        self.regulation()
+
+        if not self.cur_methods:
+            self.cur_methods.append(
+                choice(
+                    list(set(METHODS) - set(self.discarded_methods))
+                )
+            )
+
+        if self.last_move_result == 'Miss.':
+            self.cur_methods.append(
+                choice(
+                    list(set(METHODS) - set(self.cur_methods) - set(self.discarded_methods))
+                )
+            )
+
+        method = self.cur_methods[-1]
+        move = getattr(self.cursor, method)()
+        self.waits_to_discard.append(move)
+
+        return move
+
+    def random_point(self):
+        cur = Cursor()
+        nums_set = {str(num) for num in range(1, 11)}
+        available_points = list(set(cur._field_keys) - nums_set - set(self.discarded_points))
+        move = choice(available_points)
+        self.discarded_points.append(move)
+        return move
+
     def define_move(self):
-        pass
+        if self.last_move_result == 'Damaged.':
+            self.status = 'destruction'
+        elif self.last_move_result == 'Destroyed.':
+            self.cur_methods = []
+            self.discarded_methods = []
+            self.discarded_points += self.area_to_exclude + self.waits_to_discard
+            self.waits_to_discard = []
+            self.status = 'search'
+
+        if self.status == 'destruction':
+            move = self.destruction()
+        else:
+            move = self.search()
+
+        return move
 
 
 class Game:
-    player_battlefield = Battlefield()
-    opponent_battlefield = Battlefield()
-    player = Player()
-    opponent = Opponent()
-
     def __init__(self):
+        self.player_battlefield = Battlefield()
+        self.opponent_battlefield = Battlefield()
+        self.player = Player()
+        self.opponent = Opponent()
         self.start()
 
     def __player_ships_placing(self):
@@ -94,7 +197,6 @@ class Game:
 
                 self.opponent_battlefield.place_ship(ship)
 
-
     def is_destroyed_check(self, move, player):
         ships = SHIPS_EMPTY_SET
         field = getattr(self, player + '_battlefield')._field
@@ -129,7 +231,7 @@ class Game:
             for point in around_ship:
                 getattr(self, field_name).change_value(point, '.')
 
-            return result
+            return result, around_ship
 
     def player_move(self):
         move = self.player.define_move()
@@ -137,35 +239,46 @@ class Game:
         is_destroyed = self.outline_ship(move, 'opponent_battlefield', 'opponent')
 
         if is_destroyed:
-            result = is_destroyed
+            result = is_destroyed[0]
 
         return result
 
     def opponent_move(self):
         move = self.opponent.define_move()
-        self.opponent.last_move = self.player_battlefield.make_move(move)
+        self.opponent.last_move_point = move
+        self.opponent.last_move_result = self.player_battlefield.make_move(move)
         is_destroyed = self.outline_ship(move, 'player_battlefield', 'player')
 
         if is_destroyed:
-            self.opponent.last_move = is_destroyed
+            self.opponent.last_move_result = is_destroyed[0]
+            self.opponent.area_to_exclude = is_destroyed[1]
 
-        return  self.opponent.last_move
+        return self.opponent.last_move_result
 
-    def visualize_playing(self):
+    def game(self):
         os.system('cls')
         # os.system('clear')
-        print(self.opponent_battlefield)
+        print(self.player_battlefield)
         while True:
-            result = self.player_move()
+            # input()
+            time.sleep(1.0)
+            result = self.opponent_move()
+            print(result)
             os.system('cls')
             # os.system('clear')
-            print(self.opponent_battlefield)
-            print(result)
+            print(self.player_battlefield)
 
-
-
-    
     def start(self):
         # self.__player_ships_placing()
+        ships = random_ships_set()
+
+        for length in ships:
+            for ship_idx in ships[length]:
+                ship_attr_name = SHIPS_ATTR_NAMES[length] + str(ship_idx)
+                ship = ships[length][ship_idx]
+                setattr(self.player, ship_attr_name, ship)
+
+                self.player_battlefield.place_ship(ship.ship)
+
         self.opponent_ships_placing()
-        self.visualize_playing()
+        self.game()
